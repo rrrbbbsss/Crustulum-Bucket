97d5850797c083ab1eb9e8c8fbfe2cc3649af709
coleyrockin <coleyrockin@aol.com>
2022-10-26 14:53:01 -0500

    faker ew

---
+++ server/seeds/seed.js
@@ -0,0 +1,21 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { Paste, User } = require('../models');
+
+db.once('open', async () => {
+  await Paste.deleteMany({});
+  await User.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+});
+
+//Not done
