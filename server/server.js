1845e33fc588b82194b5576a838e471703007a3d
coleyrockin <coleyrockin@aol.com>
2022-10-26 14:11:55 -0500

    Error in server.js, added utils

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('apollo-server-express');
 
-const { typeDefs, resovlers } = require('./schema');
+const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection')
 
 const PORT = process.env.PORT || 3001;
@@ -38,5 +38,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
 };
 
-// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
