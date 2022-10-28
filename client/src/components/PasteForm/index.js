7a824bc4fac6d0287b4772472ff4789391c006b0
swvmpdad <will.g.camp@gmail.com>
2022-10-28 17:16:39 -0500

    fixed createPaste mutation

--- client/src/components/PasteForm/index.js
+++ client/src/components/PasteForm/index.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import { useMutation } from '@apollo/client';
-import { RefetchQueriesInclude } from "@apollo/client";
+import { Link } from "react-router-dom";
 import { CREATE_PASTE } from "../../utils/mutations";
-import { useNavigate } from "react-router-dom";
 import { QUERY_ME } from "../../utils/queries";
 
 const PasteForm = () => {
 
     const [formState, setFormState] = useState({ text: '' });
-    const [createPaste, { error }] = useMutation(CREATE_PASTE);
+    const [createPaste, { error }] = useMutation(CREATE_PASTE, {
+        update(cache, { data: { createPaste } }) {
+            
+            try {
+                const { me } = cache.readQuery({ query: QUERY_ME });
+
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, pastes: [...me.pastes, createPaste] } }
+                });
+            } catch (e) {
+                console.warn("First paste insertion by user!")
+            }
+        }
+    });
 
     const handleChange = (event) => {
         const { name, value } = event.target;
