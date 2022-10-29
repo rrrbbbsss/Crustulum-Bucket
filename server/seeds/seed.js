41ed00190c083bc902377a86e337c82b5e6233d7
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-28 20:30:10 -0500

    update seeder for changes

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,9 +1,9 @@
-const faker = require('faker');
+const faker = require("faker");
 
-const db = require('../config/connection');
-const { Paste, User } = require('../models');
+const db = require("../config/connection");
+const { Paste, User } = require("../models");
 
-db.once('open', async () => {
+db.once("open", async () => {
   await Paste.deleteMany({});
   await User.deleteMany({});
 
@@ -18,9 +18,8 @@ db.once('open', async () => {
   }
 
   const users = await User.collection.insertMany(userData);
-  const createdUsers = await User.find({})
+  const createdUsers = await User.find({});
   console.log(createdUsers);
-
 
   //Create paste // NOTE _id needed
   let createdPastes = [];
@@ -30,7 +29,7 @@ db.once('open', async () => {
     const randomUserIndex = Math.floor(Math.random() * createdUsers.length);
     const { _id: userId } = createdUsers[randomUserIndex];
 
-    const createdPaste = await Paste.create({ pasteText });
+    const createdPaste = await Paste.create({ text: pasteText, owner: userId });
 
     const updatedUser = await User.updateOne(
       { _id: userId },
@@ -39,8 +38,8 @@ db.once('open', async () => {
 
     createdPastes.push(createdPaste);
   }
-  console.log('all done!');
-  const updatedUsers = await User.find({}).populate("pastes")
+  console.log("all done!");
+  const updatedUsers = await User.find({}).populate("pastes");
   console.log(JSON.stringify(updatedUsers, null, 4));
 
   process.exit(0);
