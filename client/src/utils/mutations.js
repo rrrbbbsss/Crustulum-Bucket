7623f1546ce4dfc54dca3b08742ccd082e0911f6
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-10-29 13:38:07 -0500

    .

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -37,7 +37,7 @@ export const SIGN_UP_USER = gql`
 export const CREATE_PASTE = gql`
     mutation createPaste($input: createPasteInput!) {
         createPaste(input: $input) {
-            paste {
+            paste 
                 uuid
                 text
                 expires
