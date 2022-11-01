687355ac48eef78ecbb3dfce4c6fae0aa1804d24
coleyrockin <coleyrockin@aol.com>
2022-11-01 16:42:32 -0500

    sever

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 const { cleaner } = require("./utils/cleanup");
 const { ApolloServer } = require("apollo-server-express");
+
 const { typeDefs, resolvers } = require("./schema");
 const db = require("./config/connection");
 
@@ -40,7 +41,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
       );
     });
   });
-
 };
 
 startApolloServer(typeDefs, resolvers);
