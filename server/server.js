bc14bddef0af2fa4eb39fa31d2443c37c87817dd
coleyrockin <coleyrockin@aol.com>
2022-11-01 16:41:52 -0500

    Removing stripe from sever

--- server/server.js
+++ server/server.js
@@ -3,11 +3,8 @@ const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 const { cleaner } = require("./utils/cleanup");
 const { ApolloServer } = require("apollo-server-express");
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const { typeDefs, resolvers } = require("./schema");
 const db = require("./config/connection");
-const YOUR_DOMAIN = 'http://localhost:3000';
-
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -21,23 +18,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1Lz82THQ6q6DJNSmEj2OXvxU',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-    automatic_tax: { enabled: true },
-  });
-  res.redirect(303, session.url);
-});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
